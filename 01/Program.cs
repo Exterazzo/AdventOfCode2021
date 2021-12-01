5642a0e328e17d16c3118beb49df084bfd08d852
Jan Ooms <j.ooms@audentia.nl>
2021-12-01 20:15:34 +0100

    Excercse 1, part 1

---
+++ 01/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"C:\Projects\AdventOfCode2021\01\input.txt";
+            var input = System.IO.File.ReadAllLines(path);
+
+            var largerThanPrevious = 0;
+            var previous = int.MaxValue;
+            foreach(var s in input)
+            {
+                var i = Convert.ToInt32(s);
+                if (i > previous)
+                    largerThanPrevious++;
+
+
+                previous = i;
+            }
+
+            Console.WriteLine(largerThanPrevious);
+            Console.ReadLine();
+        }
+    }
+}
