8d0a1b7cfc89cfc16fe4cea316163e8c4361286a
Jan Ooms <j.ooms@audentia.nl>
2021-12-04 14:10:48 +0100

    Prepare for day 4

---
+++ AdventOfCode/Day04/Exercise.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using AdventOfCode.Interfaces;
+
+namespace AdventOfCode.Day04
+{
+    public class Exercise : IExercise<int, int>
+    {
+        private string[] _input;
+
+        public Exercise()
+        {
+            _input = System.IO.File.ReadAllLines("Input\\day4.txt");
+        }
+
+        public Exercise(string[] input)
+        {
+            _input = input;
+        }
+
+        public int GetFirstAnswer()
+        {
+            return 0;
+        }
+
+        public int GetSecondAnswer()
+        {
+            return 0;
+        }        
+    }
+}
