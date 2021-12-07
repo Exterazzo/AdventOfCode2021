1a99412e1fb87962eae3c2d6fa54aa8489700abf
Jan Ooms <jan@scorito.com>
2021-12-07 08:27:33 +0100

    Day 7, part 1 (preparation)

---
+++ AdventOfCode/Day07/Exercise.cs
@@ -0,0 +1,29 @@
+﻿using AdventOfCode.Interfaces;
+
+namespace AdventOfCode.Day07
+{
+    public class Exercise : IExercise<int, long>
+    {
+        private string[] _input;
+
+        public Exercise()
+        {
+            _input = System.IO.File.ReadAllLines("Input\\day7.txt");
+        }
+
+        public Exercise(string[] input)
+        {
+            _input = input;
+        }
+
+        public int GetFirstAnswer()
+        {
+            return 0;
+        }
+
+        public long GetSecondAnswer()
+        {
+            return 0;
+        }
+    }
+}
---
+++ Tests/Tests/Day07.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using AdventOfCode.Day07;
+using FluentAssertions;
+using Xunit;
+
+namespace Tests.Tests
+{
+    public class Day07
+    {
+        [Fact]
+        public void Part1()
+        {
+            // Arrange
+            var input = GetSampleInput();
+            var exercise = new Exercise(input);
+
+            // Act
+            var firstAnswer = exercise.GetFirstAnswer();
+
+            // Assert
+            firstAnswer.Should().Be(37);
+        }
+
+        [Fact]
+        public void Part2()
+        {
+            // Arrange
+            var input = GetSampleInput();
+            var exercise = new Exercise(input);
+
+            // Act
+            var secondAnswer = exercise.GetSecondAnswer();
+
+            // Assert
+            secondAnswer.Should().Be(0);
+        }
+
+        private string[] GetSampleInput()
+        {
+            var input = @"16,1,2,0,4,2,7,1,2,14";
+            return input.Split(
+                new string[] { Environment.NewLine },
+                StringSplitOptions.None
+            );
+        }
+    }
+}
